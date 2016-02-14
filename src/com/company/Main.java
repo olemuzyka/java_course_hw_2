5a651c1b329b7e9ca1270e8ccaece64ad277fb2a
olemuzyka <olemuzyka@gmail.com>
2016-02-14 10:30:20 +0200

    add array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,20 +3,31 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        char[][] arr = new char[5][5];
-        char s = '*';
-        for (int i = 0; i < arr.length; i++) {
-            int a = arr.length-1;
-            int b = -1;
-            for (int j = 0; j < arr[i].length; j++) {
-                if (i < a-- && j <= arr.length - b++) {
-                    arr[i][j] = ' ';
-                }else{
-                    arr[i][j] = '*';
-                }
-                System.out.print(arr[i][j] + " ");
+      int[]array = new int[30];
+        for (int i = 0; i <array.length ; i++) {
+            //System.out.print(i + " ");
+            array[i]=i+1;
+
+        }
+        int count = 0;
+        for (int i = 0; i <array.length ; i++) {
+            if(array[i]%2==0){
+                System.out.print(array[i] + " ");
+                count++;
             }
-            System.out.println(" ");
+
+        }
+        int[]secondArrau = new int[count];
+        int x = 0;
+        for (int i = 0; i < array.length ; i++) {
+            if (array[i]%2==0){
+                secondArrau[x]=array[i];
+                x++;
+            }
+
+        }
+        for (int z:secondArrau ) {
+            System.out.print(z + " ");
         }
     }
 }
