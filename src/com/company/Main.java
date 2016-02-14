af47e8ff118b0ac58db0b14ed4d502f75325bf03
olemuzyka <olemuzyka@gmail.com>
2016-02-14 10:08:54 +0200

    add new code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Oleg");
+        char[][] arr = new char[5][5];
+        char s = '*';
+        for (int i = 0; i < arr.length; i++) {
+            int a = arr.length-1;
+            int b = -1;
+            for (int j = 0; j < arr[i].length; j++) {
+                if (i < a-- && j <= arr.length - b++) {
+                    arr[i][j] = ' ';
+                }else{
+                    arr[i][j] = '*';
+                }
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
     }
 }
